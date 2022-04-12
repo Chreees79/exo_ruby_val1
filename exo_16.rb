452430a4f8622a851f6050845e906204f5146e2d
Chreees79 <christelle.gevaert32@gmail.com>
2022-04-12 19:53:48 +0200

    add exo_16.rb

---
+++ exo_16.rb
@@ -0,0 +1,13 @@
+puts "peux-tu me donner un chiffre en 1 et 25 ?"
+print "> "
+rows = gets.chomp.to_i
+puts "Voici la pyramide : "
+for row in 1..rows
+  (rows - row).times do
+    print " "
+  end
+  row.times do
+    print "#"
+  end
+  puts
+end
