0cb658ae0a4d2ff818976a623d32951cb21a9fd3
Chreees79 <christelle.gevaert32@gmail.com>
2022-04-12 19:48:27 +0200

    exo_11.rb

---
+++ exo_11.rb
@@ -0,0 +1,10 @@
+puts "Bonjour l'ami, peux-tu me donner ton âge ?"
+print "> "
+user_birth = gets.chomp.to_i
+x = user_birth
+y = 0
+while x >= 0
+  puts "Il y a #{x} ans, tu avais #{y} ans"
+  x -= 1
+  y += 1
+end
